6a88d9132b8c72722486ab5ce6fda91f8866ea66
hugo <phanhuyhugo@gmail.com>
2021-08-27 19:58:39 +0100

    sigma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
-import './App.css';
-import Logo from './Java-Logo.png';
-function App() {
+import React, { useState } from 'react'
+
+import './App.css'
+
+const Feedback = ({good, setGood, neutral, setNeutral, bad, setBad}) => {
   return (
-    <div className="App">
-      <h1>brogramming lol</h1>
-      <img src={Logo} alt="b ro gram i ng"></img>
+    <div className='feedback'>
+      <button className='btn btn-good' onClick={() => setGood(good+1)}>Good</button>
+      <button className='btn btn-neutral' onClick={() => setNeutral(neutral+1)}>Neutral</button>
+      <button className='btn btn-bad' onClick={() => setBad(bad+1)}>Bad</button>
+    </div>
+  );
+};
+
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <table className='statistics'>
+      <thead>
+        <tr>
+          <th>Rating</th>
+          <th>Number of Votes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Good</th>
+          <td>
+            {good}
+          </td>
+        </tr>
+        <tr>
+          <th>Neutral</th>
+          <td>
+            {neutral}
+          </td>
+        </tr>
+        <tr>
+          <th>Bad</th>
+          <td>
+            {bad}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const feedbackControls = {good, setGood, neutral, setNeutral, bad, setBad}
+  const statisticsControls = {good, neutral, bad}
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="font-bold">Sigma</h1>
+        <h2>Give feedback</h2>
+        <Feedback {...feedbackControls}/>
+        <h2>Statistics</h2>
+        <Statistics {...statisticsControls} />
+      </div>
     </div>
   );
 }
