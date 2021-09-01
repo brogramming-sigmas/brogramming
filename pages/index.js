32dfc85c861e438a72907c7fa9390b356f1f2e5d
SF90 <shivam@sfninety.com>
2021-09-01 03:09:26 +0100

    added brosapp

--- pages/index.js
+++ pages/index.js
@@ -38,24 +38,23 @@ export default function Home() {
           </Center>
           <Center display="flex" flexDirection="column" alignContent="center" className="spacer" minH="100vh" h="100%" w="100%" maxW="100vw" alignItems="center" p={8}>
             <Heading size="2xl" mb="1em" color="black" textAlign="center">What is the brocode?</Heading>
-            <Heading mb="1em" size="xl" color="#F5730A " textAlign="center">The brogramming brocode presents just a few core rules:</Heading>
+            <Heading mb="1em" size="xl" color="#F5730A " textAlign="center">The brogramming brocode presents just a few core rules: </Heading>
 
             <Heading color="black" textAlign="center" size="lg">A bro should not touch grass.</Heading>
             <Heading color="black" textAlign="center" size="lg">A bro should never put down another bro.</Heading>
             <Heading color="black" textAlign="center" size="lg">All bros must look after other bros.</Heading>
             <Heading mb="1em" color="black" textAlign="center" size="lg">A brogrammer specifically, should never use a 🤢 HTML 🤮 template.</Heading>
             <Heading mb="1em" size="xl" color="#F5730A " textAlign="center">Other rules are engrained into your brain once you chose the grindset.</Heading>
-
           </Center>
           <Center className="spacer layer-g" minH="100vh" h="100%" w="100%" maxW="100vw" alignItems="center" p={16}>
             <Box display="flex" h="100%" w="100%" justifyContent="center" alignItems="center">
               <VStack alignItems="center" w="100%">
-                <Heading color="white" mb="1em">Upcoming brovents and projects:</Heading>
+                <Heading color="white" mb="1em">Upcoming eents and brojects: </Heading>
                 <SimpleGrid columns={[1, 1, 1, 2, 2, 3]} spacing="16px">
                   <Box rounded="2xl" h="auto" w="300px" bg="white" p={8}>
                     <VStack textAlign="left" alignItems="start">
                       <Heading color="#F5730A">BroCoin</Heading>
-                      <Text color="black">The next big, non-scam crypto with absolutely zero utility. TO THE MOON! 🚀 </Text>
+                      <Text color="black">The next big, non-scam crypto with absolutely zero utility.TO THE MOON! 🚀 </Text>
                     </VStack>
                   </Box>
                   <Box rounded="2xl" h="auto" w="300px" bg="white" p={8}>
@@ -67,14 +66,19 @@ export default function Home() {
                   <Box rounded="2xl" h="auto" w="300px" bg="white" p={8}>
                     <VStack textAlign="left" alignItems="start">
                       <Heading color="#F5730A">Non Fungible Bros</Heading>
-                      <Text color="black">NFTs? Get out of here. The next big wave is bro based digital assets, and you will find them first right here.</Text>
+                      <Text color="black">NFTs?Get out of here.The next big wave is bro based digital assets, and you will find them first right here.</Text>
                     </VStack>
                   </Box>
-
                   <Box rounded="2xl" h="auto" w="300px" bg="white" p={8}>
                     <VStack textAlign="left" alignItems="start">
                       <Heading color="#F5730A">BroCon 2021</Heading>
-                      <Text color="black">BroCon 2021 will be held telepathically. You will know when it starts.  </Text>
+                      <Text color="black">BroCon 2021 will be held telepathically.You will know when it starts.</Text>
+                    </VStack>
+                  </Box>
+                  <Box rounded="2xl" h="auto" w="300px" bg="white" p={8}>
+                    <VStack textAlign="left" alignItems="start">
+                      <Heading color="#F5730A">BrosApp</Heading>
+                      <Text color="black">In light of recent events and for legal protection, Brogrammers will use encrypted communications where telepathy is not available.</Text>
                     </VStack>
                   </Box>
                 </SimpleGrid>
@@ -83,13 +87,13 @@ export default function Home() {
           </Center>
           <Box display="flex" flexDirection="column" alignContent="center" className="spacer" minH="40vh" h="100%" w="100%" maxW="100vw" alignItems="center" p={16}>
             <VStack alignItems="center">
-              <Heading mb="1em" color="#F5730A">Contacting a bro:</Heading>
+              <Heading mb="1em" color="#F5730A">Contacting a bro: </Heading>
               <Heading textAlign="center" size="lg" mb="1em" color="black">If you would like to contact a bro, simply think a thought.</Heading>
             </VStack>
             <Spacer>
             </Spacer>
 
-            <Heading textAlign="center" color="black">Brogramming. Copyright 2021.</Heading>
+            <Heading textAlign="center" color="black">Brogramming.Copyright 2021.</Heading>
 
           </Box>
         </VStack>
